7d656f34930dc26da1d45dbeb946bd223b882659
Milton Ramos <milton.ramos@avature.net>
2013-06-26 01:16:19 -0300

    agrego fk faltante

--- src/Diccionario.sql
+++ src/Diccionario.sql
@@ -136,7 +136,9 @@ CREATE TABLE StudEquino (
   FechaHasta date NULL, 
   NombreStud varchar(100) NULL,
   PRIMARY KEY (NumeroEquino, FechaDesde),
-  CONSTRAINT FK_StudEquino_NumeroEquino FOREIGN KEY (NumeroEquino) REFERENCES Equino(NumeroEquino)
+  CONSTRAINT FK_StudEquino_NumeroEquino FOREIGN KEY (NumeroEquino) REFERENCES Equino(NumeroEquino),
+  CONSTRAINT FK_StudEquino_NombreStud FOREIGN KEY (NombreStud) REFERENCES Stud(NombreStud),
+  
 );
 
 CREATE TABLE Persona (
