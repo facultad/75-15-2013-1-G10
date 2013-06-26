7646a0dfee55c8c19e39a4d6f191e53be848270a
Milton Ramos <milton.ramos@avature.net>
2013-06-26 16:39:11 -0300

    corrijo reporte

--- src/Reporte.sql
+++ src/Reporte.sql
@@ -1,25 +1,26 @@
 SELECT 
-	Andarivel.NumeroAndarivel,
-	Equino.Nombre,
-	Equnino.Peso,
-	PersonaJockey.Nombre,
-	Jockey.Peso,
-	ParticipacionCarrera.Diferencia
-	PersonaCuidador.Nombre,
-	Stud.NombreStud
+    Andarivel.NumeroAndarivel,
+    Equino.NumeroEquino,
+    ParticipacionCarrera.PesoEquino,
+    PersonaJockey.Nombre,
+    Jockey.Peso,
+    ParticipacionCarrera.DiferenciaCuerpos,
+    PersonaCuidador.Nombre,
+    Stud.NombreStud
 FROM
-	Equino, Andarivel, Jockey, Persona AS PersonaJockey, Rol, Cuida, Persona AS PersonaCuidador, Stud, ParticipacionCarrera, StudEquino
+    Equino, Andarivel, Jockey, Cuida, Persona AS PersonaJockey, Rol AS RolCuidador, Rol AS RolJockey, Persona AS PersonaCuidador, Stud, ParticipacionCarrera, StudEquino
 WHERE
-	Equnino.NumeroEquino = ParticipacionCarrera.NumeroEquino
-	AND (
-		Jockey.NumeroLicenciaJockey = ParticipacionCarrera.NumeroLicenciaJockey 
-		AND Jockey.TipoLicenciaJockey = ParticipacionCarrera.TipoLicenciaJockey
-	)
-	AND Jockey.DNI = PersonaJockey.DNI
-	AND Cuida.DNI = PersonaCuidador.DNI
-	AND Cuida.NumeroEquino = Equino.NumeroEquino
-	AND (Rol.NumeroRol = Cuida.NumeroRol AND Rol.FechaDesde IS NULL)
-	AND (Andarivel.NombreHipodromo = PariticipaCarrera.NombreHipodromo AND Andarivel.NumeroPista = PariticipaCarrera.NumeroPista)
-	AND (StudEquino.NumeroEquino = Equino.NumeroEquino AND StudEquino.FechaHasta IS NULL)
-	AND StudEquino.NombreStud = Stud.NombreStud
+    Equino.NumeroEquino = ParticipacionCarrera.NumeroEquino
+    AND (
+        Jockey.NumeroLicenciaJockey = ParticipacionCarrera.NumeroLicenciaJockey 
+        AND Jockey.TipoLicenciaJockey = ParticipacionCarrera.TipoLicenciaJockey
+    )
+    AND RolJockey.DNI = PersonaJockey.DNI
+    AND Jockey.NumeroRol = RolJockey.NumeroRol
+    AND RolCuidador.DNI = PersonaCuidador.DNI
+    AND (RolCuidador.NumeroRol = Cuida.NumeroRol AND RolCuidador.FechaDesde IS NULL)
+    AND Cuida.NumeroEquino = Equino.NumeroEquino
+    AND (Andarivel.NumeroAndarivel = ParticipacionCarrera.NumeroAndarivel)
+    AND (StudEquino.NumeroEquino = Equino.NumeroEquino AND StudEquino.FechaHasta IS NULL)
+    AND StudEquino.NombreStud = Stud.NombreStud
 
