aa14847e3ff43bf5ca885cff569b9fa484c6239e
Sebastian <sebas.goldberg@gmail.com>
2013-06-26 11:14:12 -0700

    Se corrige sintaxis condición E y se agrega creación de base de datos al diccionario

--- src/CondicionE.sql
+++ src/CondicionE.sql
@@ -23,28 +23,28 @@ WHERE
     AND (StudEquino.NumeroEquino = Equino.NumeroEquino AND StudEquino.FechaHasta IS NULL)
     AND 12 > (
    		SELECT COUNT(*) 
-		FROM ParticipacionCarrera PC1, StudEquino SE1
-		WHERE 
-			PC1.NumeroEquino = SE1.NumeroEquino
-			PC1.TiempoCarrera = (
-				SELECT MIN(TiempoCarrera) 
-				FROM ParticipacionCarrera PC3
-				WHERE 
-          PC3.NumeroCarrra = PC1.NumeroCarrra AND
-          PC3.FechaEncuentro = PC1.FechaEncuentro
-			)
-		GROUP BY SE2.NombreStud
-		HAVING COUNT(*) > (
-			SELECT COUNT(*) 
-			FROM ParticipacionCarrera PC2, StudEquino SE2
-			WHERE 
-				SE2.NombreStud = StudEquino.NombreStud
-				PC2.NumeroEquino = SE2.NumeroEquino
-				PC2.TiempoCarrera = (
-					SELECT MIN(TiempoCarrera) 
-					FROM ParticipacionCarrera PC4
-					WHERE PC4.NumeroCarrra = PC2.NumeroCarrra AND PC4.FechaEncuentro = PC2.FechaEncuentro
-				)
-			GROUP BY SE2.NombreStud
-		)
-    )
+  		FROM ParticipacionCarrera PC1, StudEquino SE1
+	  	WHERE 
+		  	PC1.NumeroEquino = SE1.NumeroEquino and
+			  PC1.TiempoCarrera = (
+				  SELECT MIN(TiempoCarrera) 
+  				FROM ParticipacionCarrera PC3
+	  			WHERE 
+            PC3.NumeroCarrera = PC1.NumeroCarrera AND
+            PC3.FechaEncuentro = PC1.FechaEncuentro
+			  )
+  		GROUP BY SE1.NombreStud
+	  	HAVING COUNT(*) > (
+		  	SELECT COUNT(*) 
+  			FROM ParticipacionCarrera PC2, StudEquino SE2
+	  		WHERE 
+		  		SE2.NombreStud = StudEquino.NombreStud and
+			  	PC2.NumeroEquino = SE2.NumeroEquino and
+			  	PC2.TiempoCarrera = (
+				  	SELECT MIN(TiempoCarrera) 
+				  	FROM ParticipacionCarrera PC4
+				  	WHERE PC4.NumeroCarrera = PC2.NumeroCarrera AND PC4.FechaEncuentro = PC2.FechaEncuentro
+				  )
+			  GROUP BY SE2.NombreStud
+		  )
+      )
